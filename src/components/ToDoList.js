69373e3e40a7f0cf982dd9099d321a46e544a806
Irene <irene.bhuiyan@gmail.com>
2019-07-10 16:16:05 +0100

    added bootstrap and styling

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -29,16 +29,38 @@ export default class ToDoList extends Component {
   render(){
     return(
       <div className="to-do-list-main">
-        <input
-          onChange={ (e)=>this.changeUserInput(e.target.value)}
-          value={this.state.userInput}
-          type="text"
-        />
-      <button onClick={()=> this.addToList(this.state.userInput)}>Press Me</button>
-
-      <ul>
-        {this.state.list.map((val)=><li>{val}</li>)}
-      </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h1>To Do</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="task-input">
+                <h2>Create a Task</h2>
+              <input
+                    onChange={ (e)=>this.changeUserInput(e.target.value)}
+                    placeholder="Your task"
+                    rows="1"
+                    type="text"
+                    value={this.state.userInput}
+                  /><br/><br/>
+                <button onClick={()=> this.addToList(this.state.userInput)}>Done</button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="task-list">
+                <h2>Your Tasks</h2>
+                <ul>
+                  {this.state.list.map((val)=><li><span className="checkbox"></span>{val}</li>)}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
